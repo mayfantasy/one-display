b1b463d8a0e7442b855bd963407c9bb428bc3a34
hans-yang <hans@mayfantasy.com>
2022-02-04 08:00:37 -0500

    Added google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,23 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          {/* Global site tag (gtag.js) - Google Analytics  */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-FPKF2ZKFKG"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FPKF2ZKFKG');`,
+            }}
+          ></script>
+        </Head>
         <body className="loading">
           <Main />
           <NextScript />
